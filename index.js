039667a16fbe1cc2d7960366606aca99188253ae
costcowholesale <christianshin123@gmail.com>
2022-08-25 12:16:00 -0700

    save 2

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+const hint = document.querySelector(".hint")
+const reset = document.querySelector(".reset")
 const gameresult = document.querySelector(".gameresult")
 const gameinput = document.querySelector(".gameinput")
 const submitbutton = document.querySelector(".submitbutton")
-let answer = 25;
-let counter = Math.floor(Math.random() * 101)
-const hint = document.querySelector(".hint")
-const reset = document.querySelector(".reset")
+let tally = document.querySelector(".tally")
+let answer = Math.floor(Math.random() * 101);
+let counter = 6
+
+
+
+
 submitbutton.addEventListener('click', function () {
     counter--
+    tallyCounter(counter);
     if (counter === 0) {
         document.body.style.backgroundColor = "red";
         return gameresult.innerText = "You're out of tries";
     }
+
     if (gameinput.value > answer) {
         return hint.innerText = "hint: Too High";
     } else if (gameinput.value < answer) {
@@ -18,21 +25,26 @@ submitbutton.addEventListener('click', function () {
 
     }
 
-
     console.log(gameinput.value);
     if (answer == gameinput.value) {
         document.body.style.backgroundColor = "green";
         return gameresult.innerText = "Correct";
-    } 
+    }
 })
+
+function tallyCounter(counter) {
+    if (counter) {
+        return tally.innerText = "number of tries:" + counter
+    }
+}
+
 reset.addEventListener('click', function () {
+    tally.innerText = "number of tries:" + 5
+    counter = 5
+    document.body.style.backgroundColor = "white";
+    answer = Math.floor(Math.random() * 101)
+    return gameresult.innerText = "You have restarted";
 
- counter = 5
- document.body.style.backgroundColor = "white";
- answer = Math.floor(Math.random() * 101)
- return gameresult.innerText = "You have restarted";
-        
 })
 
 
-Math.floor(Math.random() * 101);
